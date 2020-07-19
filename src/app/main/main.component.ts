91e6d60110b9f0fe4501792e20aca4270b238e32
ronabet <ronabet456@gmail.com>
2020-07-19 20:00:47 +0300

    remove hardcoded

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -13,48 +13,14 @@ export class MainComponent implements OnInit {
   isClinicConfinement: boolean = false; 
   wasExported: boolean;
   workFinished: boolean;
-  clinicNotes: string=''; 
+  clinicNotes: string = ''; 
   hamalNotes: string;
-  reporterDetails: {
-    reporterIdentityNumber: string;
-    reporterFirstName: string;
-    reporterLastName: string;
-    reporterMail: string;
-    reporterPhoneNumber: string;
-    reporterNotes: string;
-    isColoneConfinement: boolean
-
-  }={
-    reporterIdentityNumber: '111111111',
-    reporterFirstName: 'firstName',
-    reporterLastName: 'lastName',
-    reporterMail: 'name@gmail.com',
-    reporterPhoneNumber: '050-0000000',
-    reporterNotes: 'reporter notes',
-    isColoneConfinement: false
-  };
-  suspectDetails: {
-    suspectIdentityNumber: string;
-    suspectFirstName: string;
-    suspectLastName: string;
-    suspectPhoneNumber: string;
-    suspectTimeOfPositioning: Date;
-    suspectPlaceOfPositioning: string;
-  suspectHasDeclaredInocense: boolean; 
-  } = {
-    suspectIdentityNumber: '123456789',
-    suspectFirstName: 'niv',
-    suspectLastName: 'konforty',
-    suspectPhoneNumber: '052-454442',
-    suspectTimeOfPositioning: new Date(),
-    suspectPlaceOfPositioning: 'tel aviv',
-    suspectHasDeclaredInocense: true
-  };
+  isColoneConfinement: boolean = true;
 
   constructor(@Inject(MAT_DIALOG_DATA) public SuspectData: any) {}
 
   ngOnInit() {
-    console.log(this.SuspectData);
+    
   }
 
   ifSuspectWasInBaseDuringPas() {
