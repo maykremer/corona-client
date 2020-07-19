3523547483e3515fe10edda67b579b09e6b7b8bc
may kremer <maykremer@gmail.com>
2020-07-19 23:15:45 +0300

    add excel

---
+++ src/app/excel.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+// import { type } from 'os';
+
+const EXCEL_TYPE =
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+const EXCEL_EXTENSION = '.xlsx';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExcelService {
+  constructor() {}
+
+  public exportAsExcelFile(json: any[], excelFileName: string): void {
+    const workSheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+    const workBook: XLSX.WorkBook = {
+      Sheets: { data: workSheet },
+      SheetNames: ['data'],
+    };
+    const excelBuffer: any = XLSX.write(workBook, {
+      bookType: 'xlsx',
+      type: 'array',
+    });
+    this.saveAsExcelFile(excelBuffer, excelFileName);
+  }
+
+  private saveAsExcelFile(buffer: any, fileName: string) {
+    const data: Blob = new Blob([buffer], { type: EXCEL_TYPE });
+    FileSaver.saveAs(
+      data,
+      fileName + EXCEL_EXTENSION);
+  }
+}
+///////
